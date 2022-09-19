763d380a8a010573e51f92e42cd853224924966c
litarobert <robert.l@adoreme.com>
2022-09-19 11:23:36 +0300

    new test added

--- src/test_even.py
+++ src/test_even.py
@@ -7,7 +7,7 @@ class TestClass:
         assert isEven(12312) == True
         assert isEven(5) == False
         assert isEven(9) == False
-        assert isEven(15) == False
+        assert isEven(12) == False
 
     def test_two(self):
         assert isEven(-5) == False
